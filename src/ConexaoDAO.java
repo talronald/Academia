35ae75d5e6df4e9932cbdcb5886ec7e28649dd9b
talronald <talronald2002@gmail.com>
2023-11-10 20:41:41 -0300

    primeiro commit: conexao banco de dados

---
+++ src/ConexaoDAO.java
@@ -0,0 +1,22 @@
+
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoDAO {
+    
+    public Connection conectaDB(){
+        Connection conn = null;
+        
+        try {
+            String url = "jdbc:mysql://localhost:3306/bancoacad?user=root&password=";
+            conn = DriverManager.getConnection(url);
+            
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao conectar no banco de dados"+erro.getMessage());
+        }
+        return conn;
+    }
+    
+}
